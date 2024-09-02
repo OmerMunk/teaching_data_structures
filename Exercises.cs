6d68e88bf4d46d48f63e300007970400d55b9d44
Omer Munk <omermunk95@gmail.com>
2024-09-02 18:07:43 +0300

    update

--- Exercises.cs
+++ Exercises.cs
@@ -12,13 +12,23 @@ public class Exercises
         return copy;
     }
     
+    // [1, 2, 3]
     public static int[] Ex1ReverseArray(int[] array)
     {
-        int[] reversed = new int[array.Length];
+        int[] reversed = new int[array.Length]; // 1
+        //       1            4            3
         for (int i = 0; i < array.Length; i++)
         {
-            reversed[i] = array[array.Length - 1 - i];
+            // 3
+            reversed[i] = array[array.Length - 1 - i]; // 1
         }
-        return reversed;
+        return reversed; // 1
+        // n = 3:  1 + 1 + 4 +    n +    n    + 1 = 13
+        // n = 4:  1 + 1 + 5 +    n +    n    + 1 = 16
+        // n = 5:  1 + 1 + 6 +    n +    n    + 1 = 19
+        
+        // O(n)
+        
+        // n!
     }
 }
