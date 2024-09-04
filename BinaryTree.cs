afecf74d7fc9090761480e3e7939f7f2366e336f
Omer Munk <omermunk95@gmail.com>
2024-09-04 15:16:43 +0300

    start of binary serach tree

---
+++ BinaryTree.cs
@@ -0,0 +1,16 @@
+namespace data_structures;
+
+public class BinaryTree
+{
+    private TreeNode root;
+
+    public BinaryTree()
+    {
+        root = null;
+    }
+
+    public void Insert(int value)
+    {
+        
+    }
+}
