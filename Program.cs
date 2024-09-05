a0a8767faea20ba04ae84437ed39099337c98525
Omer Munk <omermunk95@gmail.com>
2024-09-05 10:41:18 +0300

    for determine c# versions

--- Program.cs
+++ Program.cs
@@ -82,12 +82,13 @@
 // 2     [_, _, _]
 // ]
 
-char[][] chars = new char[3][];
+char[,] chars = new char[3,3];
+
 for (int i = 0; i < 3; i++)
 {
     for (int j = 0; j < 3; j++)
     {
-        chars[i][j] = '_';
+        chars[i,j] = '_';
     }
 }
 
